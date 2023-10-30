76563c142556b152097c40f86bd4d65daa258e36
James-Canova <jscanova@gmail.com>
2023-10-29 18:55:51 -0700

    Updating SW

---
+++ Software/libraries/Robot_Library/src/Display.cpp
@@ -0,0 +1,28 @@
+//PairValues.cpp
+
+#include "PairValues.h"
+
+
+void  PairValues::SetValues(float fValue1, float fValue2)
+{
+	m_fValue1 = fValue1;  
+	m_fValue2 = fValue2;		
+}
+
+
+float PairValues::GetValue1()
+{	
+	return m_fValue1;
+}
+
+float PairValues::GetValue2()
+{	
+	return m_fValue2;
+}
+
+
+
+
+
+
+
